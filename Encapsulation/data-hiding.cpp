2801ce7ac82e79c7d722ec799e866db720edc09f
Vaish Mamulkar <vaishmamulkar@gmail.com>
2025-12-02 17:30:10 +0530

    initial commit

---
+++ Encapsulation/data-hiding.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int marks;   // hidden data
+
+public:
+    void setMarks(int m) {   // controlled access
+        if(m >= 0 && m <= 100)
+            marks = m;
+        else
+            marks = 0;
+    }
+
+    int getMarks() {         // safe read
+        return marks;
+    }
+};
+
+int main() {
+    Student s;
+    // s.marks = 90;   // ❌ Not allowed: data is hidden (private)
+    s.setMarks(95);    // ✔ allowed through public function
+    cout << s.getMarks();
+}
