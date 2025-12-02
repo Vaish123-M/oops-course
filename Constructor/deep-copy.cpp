271d491a61e4ab425f0e2ce85cef0a8b7ded0ec8
Vaish Mamulkar <vaishmamulkar@gmail.com>
2025-12-02 20:13:15 +0530

    added deep copy

---
+++ Constructor/deep-copy.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Demo {
+public:
+    int *ptr;
+
+    Demo(int x) {
+        ptr = new int(x);
+    }
+
+    // Deep Copy Constructor
+    Demo(const Demo &d) {
+        // Allocates NEW memory for this object and copies the value
+        ptr = new int(*d.ptr);   // ✔ new memory, safe
+    }
+};
+
+int main() {
+    Demo obj1(10);
+    Demo obj2 = obj1;      // Deep copy called
+
+    cout << *obj1.ptr << endl;   // 10
+    cout << *obj2.ptr << endl;   // 10
+
+    *obj2.ptr = 50;         // changes only obj2
+
+    cout << *obj1.ptr << endl;   // ✔ still 10
+    cout << *obj2.ptr << endl;   // 50
+
+    return 0;
+}
